747cf56126fce06d1668bd4be1e9768749a0e87f
James Weakley <jameswillisweakley@gmail.com>
2024-04-08 07:36:10 +1000

    More improvements and fixes

---
+++ close_door.py
@@ -0,0 +1,16 @@
+import rpyc
+from hardware_service import HardwareService
+
+def get_hardware_service() -> HardwareService:
+    hardware_service:HardwareService = rpyc.connect("localhost", 18861).root
+    return hardware_service
+
+hs = get_hardware_service()
+hs.close_door()
+hs.capture_camera_image()
+
+from pushover import Client
+client = Client("ueovdt7awncixwx7bgoiz8fp6p9jrr", api_token="am62ab28fb7p389h15v5nnjmcj63oo")
+#client.send_message("Hello!", title="Hello")
+with open('/tmp/camera.jpg', 'rb') as image:
+    client.send_message('Door closed', attachment=image)
